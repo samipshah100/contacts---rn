aa5a34c2f8e72f21bf203ea116a9e5466174896f
Samip Shah <samip227@gmail.com>
2019-08-04 11:19:56 +0530

    flatlist

--- App.js
+++ App.js
@@ -2,31 +2,42 @@ import React from 'react';
 import { Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 
-import contacts from './contacts'
+import contacts, {compareNames} from './contacts'
 
 import Row from './Row'
 
 export default class App extends React.Component {
   state = {
     showContacts: false,
+    contacts: contacts,
   }
 
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  sort = () => {
+    this.setState(
+      prevState => ({
+        contacts: [...prevState.contacts].sort(compareNames),
+      })
+    )
+  }
+
+  renderItem = (obj) => (
+    <Row {...obj.item} />
+  )
+
   render() {
     return (
       <View style={styles.container}>
         <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort" onPress={this.sort} />
         {this.state.showContacts && ( 
-          <ScrollView>
-            {contacts.map(
-              a => (
-                <Row {... a}/>
-              )
-            )}
-          </ScrollView>
+          <FlatList
+          data = {this.state.contacts}
+          renderItem = {this.renderItem}
+          />
         )
         }
       </View>
