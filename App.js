9328c13f7db7cf6afbd2da16f22f3e0015b3c2c8
Samip Shah <samip227@gmail.com>
2019-08-10 14:16:09 +0530

    end of navigation

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Button, SectionList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import AddContactForm from './AddContactForm'
-import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation'
+import { createAppContainer, createStackNavigator, createSwitchNavigator, createBottomTabNavigator } from 'react-navigation'
 import AddContactScreen from './screens/AddContactScreen'
 import ContactListScreen from './screens/ContactListScreen'
 import ContactDetailsScreen from './screens/ContactDetailsScreen'
 import LoginScreen from './screens/LoginScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import { Ionicons } from '@expo/vector-icons';
+
 
 import contacts, { compareNames } from './contacts'
 
@@ -40,7 +43,7 @@ export default class App extends React.Component {
 }
 
 
-const MainNavigator = createStackNavigator({
+const ContactsTab = createStackNavigator({
   AddContact: AddContactScreen,
   ContactList: ContactListScreen,
   ContactDetails: ContactDetailsScreen,
@@ -53,6 +56,28 @@ const MainNavigator = createStackNavigator({
 },
 )
 
+ContactsTab.navigationOptions = {
+  tabBarIcon: ({ focused, tintColor }) => (
+    <Ionicons
+      name={`${focused ? "ios-contacts" : "ios-contact"}`}
+      size={25}
+      color={tintColor}
+    />
+  )
+}
+
+const MainNavigator = createBottomTabNavigator(
+  {
+    Contacts: ContactsTab,
+    Settings: SettingsScreen,
+  },
+  {
+  tabBarOptions: {
+    activeTintColor: "#a41034",
+  },
+
+  },
+)
 const AppNavigator = createSwitchNavigator(
   {
     Main: MainNavigator,
@@ -60,6 +85,9 @@ const AppNavigator = createSwitchNavigator(
   },
   {
     initialRouteName: "Login",
+    navigationOptions: {
+      headerTintColor: "#a41034",
+    },
   }
 )
 
